7b4220af24e851aa05e3651f052fe7f9da4c6cd5
Noppadon Lee <noppadon.lee@gmail.com>
2014-10-02 13:51:00 +0700

    solved challenge

--- UIScrollView Challenge Solution/ViewController.h
+++ UIScrollView Challenge Solution/ViewController.h
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIScrollViewDelegate>
 
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) UIImageView *globalImageView;
 
 @end
 
